ca15907770969266e51c3fb217b21314a8482e26
221B447 <221B447@juet.co.in>
2025-01-30 14:39:32 +0530

    adding LE1

---
+++ LE1/Prob 1/Mother.java
@@ -0,0 +1,7 @@
+class Mother {
+    int x;
+    void show() {
+        
+    }
+    
+}
---
+++ LE1/Prob 4/One.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class One
+{
+	public static void main(String[] args) {
+		One o=new One();
+		o.One(2);
+	}
+	void One(int x){
+	    System.out.println(x);
+	}
+}
